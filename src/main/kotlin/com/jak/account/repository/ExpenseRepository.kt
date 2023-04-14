41d3f3bf038b23d786ff057bb00dc632623d7c28
jaksimsamil <>
2023-04-14 17:37:01 +0900

    add view expense data

--- src/main/kotlin/com/jak/account/repository/ExpenseRepository.kt
+++ src/main/kotlin/com/jak/account/repository/ExpenseRepository.kt
@@ -1,10 +1,10 @@
 package com.jak.account.repository
 
 import com.jak.account.dto.Expense
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ExpenseRepository: CrudRepository<Expense, Int> {
+interface ExpenseRepository: JpaRepository<Expense, Int> {
         fun findAllBy(): List<Expense>
 }
